617c278c3a6c55ec2d2c884a799762e406301628
Andrew Perrault <andrew.perrault@yahoo.ca>
2017-10-11 18:07:23 -0400

    Added ACES tmo

--- tmo/aces.go
+++ tmo/aces.go
@@ -6,14 +6,14 @@ import (
 	"math"
 
 	"github.com/Xyzyx101/hdr"
-	"gonum.org/v1/gonum/mat"
+	"github.com/go-gl/mathgl/mgl64"
 )
 
 // ACES is an implementation of the Academy Color Encoding System http://www.oscars.org/science-technology/sci-tech-projects/aces
 // This is a simple version I got from here https://github.com/TheRealMJP/BakingLab/blob/master/BakingLab/ACES.hlsl
 type ACES struct {
-	HDRImage hdr.Image
-	Gamma    float64
+	HDRImage     hdr.Image
+	ExposureBias float64
 }
 
 // NewDefaultACES return an ACES tmo with default gamma of 1.8
@@ -22,10 +22,10 @@ func NewDefaultACES(img hdr.Image) *ACES {
 }
 
 // NewACES instantates a ACES tone mapper
-func NewACES(img hdr.Image, gamma float64) *ACES {
+func NewACES(img hdr.Image, exposureBias float64) *ACES {
 	return &ACES{
-		HDRImage: img,
-		Gamma:    gamma,
+		HDRImage:     img,
+		ExposureBias: exposureBias,
 	}
 }
 
@@ -41,8 +41,8 @@ func (t *ACES) tonemap(img *image.RGBA64) {
 		for x := 0; x < img.Bounds().Dx(); x++ {
 			inPix := t.HDRImage.HDRAt(x, y)
 			r, g, b, _ := inPix.HDRRGBA()
-			colorVec := acesFitted(rgbToVec(r, g, b))
-			rgb := t.linearToRGB(colorVec)
+			colorVec := acesFitted(mgl64.Vec3{r, g, b})
+			rgb := linearToRGB(colorVec.Mul(t.ExposureBias))
 			img.SetRGBA64(x, y, color.RGBA64{
 				R: rgb[0],
 				G: rgb[1],
@@ -53,71 +53,84 @@ func (t *ACES) tonemap(img *image.RGBA64) {
 	}
 }
 
-var acesInputMat = mat.NewDense(3, 3, []float64{
-	0.59719, 0.35458, 0.04823,
-	0.07600, 0.90834, 0.01566,
-	0.02840, 0.13383, 0.83777,
-})
+// Matrices is transposed from the example to work with the matrix library
 
-var acesOutputMat = mat.NewDense(3, 3, []float64{
-	1.60475, -0.53108, -0.07367,
-	-0.10208, 1.10813, -0.00605,
-	-0.00327, -0.07276, 1.07602,
-})
+// var acesInputMat = mgl64.Mat3{
+// 	0.59719, 0.35458, 0.04823,
+// 	0.07600, 0.90834, 0.01566,
+// 	0.02840, 0.13383, 0.83777,
+// }
 
-func rgbToVec(r, g, b float64) *mat.VecDense {
-	return mat.NewVecDense(3, []float64{r, g, b})
+var acesInputMat = mgl64.Mat3{
+	0.59719, 0.07600, 0.02840,
+	0.35458, 0.90834, 0.13383,
+	0.04823, 0.01566, 0.83777,
 }
 
-func acesFitted(color *mat.VecDense) *mat.VecDense {
-	color.MulVec(acesInputMat, color)
+// var acesOutputMat = mgl64.Mat3{
+// 	1.60475, -0.53108, -0.07367,
+// 	-0.10208, 1.10813, -0.00605,
+// 	-0.00327, -0.07276, 1.07602,
+// }
+
+var acesOutputMat = mgl64.Mat3{
+	1.60475, -0.10208, -0.00327,
+	-0.53108, 1.10813, -0.07276,
+	-0.07367, -0.00605, 1.07602,
+}
+
+func acesFitted(color mgl64.Vec3) mgl64.Vec3 {
+	color = acesInputMat.Mul3x1(color)
 	color = rttAnODTFit(color)
-	color.MulVec(acesOutputMat, color)
+	color = acesOutputMat.Mul3x1(color)
 	color = saturate(color)
 	return color
 }
 
-func rttAnODTFit(x *mat.VecDense) *mat.VecDense {
-	var a mat.VecDense
-	a.AddVec(x, mat.NewVecDense(3, []float64{0.0245786, 0.0245786, 0.0245786}))
-	a.MulElemVec(x, &a)
-	a.AddVec(&a, mat.NewVecDense(3, []float64{-0.000090537, -0.000090537, -0.000090537}))
-	//b := mat.NewVecDense(3, []float64{0, 0, 0})
-	var b mat.VecDense
-	b.ScaleVec(0.983729, x)
-	b.AddVec(&b, mat.NewVecDense(3, []float64{0.4329510, 0.4329510, 0.4329510}))
-	b.MulElemVec(x, &b)
-	b.AddVec(&b, mat.NewVecDense(3, []float64{0.238081, 0.238081, 0.238081}))
-	a.DivElemVec(&a, &b)
-	//return mat.NewVecDense(3, []float64{a.At(0, 0) / b.At(0, 0), a.At(1, 0) / b.At(1, 0), a.At(2, 0) / b.At(2, 0)})
-	return &a
+func rttAnODTFit(x mgl64.Vec3) mgl64.Vec3 {
+	a := x.Add(mgl64.Vec3{0.0245786, 0.0245786, 0.0245786})
+	a[0] *= x[0]
+	a[1] *= x[1]
+	a[2] *= x[2]
+	a = a.Add(mgl64.Vec3{-0.000090537, -0.000090537, -0.000090537})
+
+	b := x.Mul(0.983729)
+	b = b.Add(mgl64.Vec3{0.4329510, 0.4329510, 0.4329510})
+	b[0] *= x[0]
+	b[1] *= x[1]
+	b[2] *= x[2]
+	b = b.Add(mgl64.Vec3{0.238081, 0.238081, 0.238081})
+
+	return mgl64.Vec3{a[0] / b[0], a[1] / b[1], a[2] / b[2]}
 }
 
-func saturate(x *mat.VecDense) *mat.VecDense {
-	x.SetVec(0, math.Max(0.0, math.Min(x.At(0, 0), 1.0)))
-	x.SetVec(1, math.Max(0.0, math.Min(x.At(1, 0), 1.0)))
-	x.SetVec(2, math.Max(0.0, math.Min(x.At(2, 0), 1.0)))
-	return x
+func saturate(in mgl64.Vec3) mgl64.Vec3 {
+	return mgl64.Vec3{
+		math.Max(0.0, math.Min(in[0], 1.0)),
+		math.Max(0.0, math.Min(in[1], 1.0)),
+		math.Max(0.0, math.Min(in[2], 1.0)),
+	}
 }
 
-func (t *ACES) linearToRGB(linear *mat.VecDense) []uint16 {
-	linear.ScaleVec(t.Gamma, linear)
-	var x mat.VecDense
-	x.ScaleVec(12.95, linear)
-	linear = saturate(linear)
-	y := mat.NewVecDense(3, []float64{
-		math.Pow(linear.At(0, 0), 1.0/2.4),
-		math.Pow(linear.At(1, 0), 1.0/2.4),
-		math.Pow(linear.At(2, 0), 1.0/2.4),
-	})
-	y.ScaleVec(1.055, y)
-	y.AddVec(y, mat.NewVecDense(3, []float64{-0.055, -0.055, -0.055}))
+const linCurve = 1.0 / 2.4
+
+func linearToRGB(linear mgl64.Vec3) []uint16 {
+	x := linear.Mul(12.92)
+
+	y := saturate(linear)
+	y = mgl64.Vec3{
+		math.Pow(y[0], linCurve),
+		math.Pow(y[1], linCurve),
+		math.Pow(y[2], linCurve),
+	}
+	y = y.Mul(1.055)
+	y = y.Add(mgl64.Vec3{-0.055, -0.055, -0.055})
 	rgb := make([]uint16, 3, 3)
 	for i := range rgb {
-		if linear.At(i, 0) < 0.0031308 {
-			rgb[i] = uint16(x.At(i, 0) * RangeMax)
+		if linear[i] < 0.0031308 {
+			rgb[i] = uint16(x[i] * RangeMax)
 		} else {
-			rgb[i] = uint16(y.At(i, 0) * RangeMax)
+			rgb[i] = uint16(y[i] * RangeMax)
 		}
 	}
 	return rgb
